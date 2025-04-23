d1aaf5d87cf8b4e9e2115fe6d7d07d6f78997988
Elilaw179 <sundayelisha179@gmail.com>
2025-04-23 18:43:57 +0100

    working on the responsivenes for the nav logo

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -4,7 +4,7 @@ import faq_icon from "../../assets/faq.png";
 import logo_icon from "../../assets/logo-h.png";
 import male_icon from "../../assets/male_user.png";
 import anounce_icon from "../../assets/anounce.png";
-import email_icon from "../../assets/email.png";
+import list_icon from "../../assets/bluelist.png";
 
 const Navbar = () => {
   return (
@@ -17,18 +17,20 @@ const Navbar = () => {
         <div className="nav-icons">
           <p>Refer & earn up to 450 USD</p>
           <ul>
-            <li>
-              <img src={anounce_icon} alt="" />
-              <span className="cart-value">1</span>
-            </li>
+          <div className="big-srn-navs-icons">
             <li>
               <img src={faq_icon} alt="" />
             </li>
             <li >
               <img src={male_icon} alt="" />
             </li>
+          </div>
+          <li>
+              <img src={anounce_icon} alt="" />
+              <span className="cart-value">1</span>
+            </li>
             <li >
-              <img  src={email_icon} alt="" />
+              <img  src={list_icon} alt="" />
             </li>
           </ul>
         </div>
